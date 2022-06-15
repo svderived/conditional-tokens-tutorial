a5a3e7acc7afe8b94a2edc55b7cbdacdf6e36f04
svderived <svderived@gmail.com>
2022-06-14 22:51:55 -0700

    new question

--- markets.config.js
+++ markets.config.js
@@ -1,16 +1,16 @@
 module.exports = [
   {
-    questionId: "0x4b22fe478b95fdaa835ddddf631ab29f12900b62061e0c5fd8564ddb7b684333",
-    title: "Will the summer 2020 in Germany break again weather records? ",
+    questionId: '0x4b22fe478b95fdaa835ddddf631ab29f12900b62061e0c5fd8564ddb7b684111',
+    title: 'Will BTC cross 50k in December 2022 ? ',
     outcomes: [
       {
-        title: "Yes",
-        short: "Yes"
+        title: 'Yes',
+        short: 'Yes',
       },
       {
-        title: "No",
-        short: "No"
-      }
-    ]
-  }
-];
+        title: 'No',
+        short: 'No',
+      },
+    ],
+  },
+]
